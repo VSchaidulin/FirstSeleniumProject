a164b2fa7e4370c69c7e0a5ad99097ce905e7d17
VSchaidulin <vaclav1981@gmx.de>
2026-03-13 17:11:33 +0100

    Hw13.1

---
+++ src/test/java/com/ait/tests/FindElementsTests.java
@@ -0,0 +1,76 @@
+package com.ait.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class FindElementsTests {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://icarro-v1.netlify.app/search?page=0&size=10");
+        driver.manage().window().maximize();
+        //driver.manage().window().setSize(new Dimension(900,400));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));// для тяжёлых сайтов
+    }
+
+    @Test
+    public void findElementByTagNameTest() {
+        //  поиск WEB ELEMENT
+
+        //поиск элемента по тексту стратегия ByTagName h1
+        WebElement element = driver.findElement(By.tagName("h1"));
+        System.out.println(element.getText());
+        //поиск элемента по тексту стратегия ByTagName h2
+        WebElement element1 = driver.findElement(By.tagName("h2"));
+        System.out.println(element1.getText());
+        //поиск элемента по тексту стратегия ByTagName a
+        WebElement link = driver.findElement(By.tagName("a"));
+        System.out.println(link.getText());
+
+
+        //поиск WEB elementS
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+        //поиск WEB elementS
+        WebElement label = driver.findElement(By.tagName("label"));
+        System.out.println(label.getText());
+
+
+    }
+    @Test
+    public void findElementById(){
+        WebElement city = driver.findElement(By.id("city"));
+        System.out.println(city.getDomAttribute("id"));
+
+        WebElement dates = driver.findElement(By.id("dates"));
+        System.out.println(dates.getDomAttribute("id"));
+
+    }
+    @Test
+    public void findElementByClassName(){
+        WebElement telephone = driver.findElement(By.className("telephone"));
+        System.out.println(telephone.getText());
+
+        WebElement search = driver.findElement(By.className("navigation-link"));
+        System.out.println(search.getText());
+        System.out.println(search.getDomAttribute("class"));
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        driver.quit();
+    }
+}
