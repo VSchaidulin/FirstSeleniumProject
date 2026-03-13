fdb6c5bcdf293a6ea6245339bb7d88ae62a86e33
VSchaidulin <vaclav1981@gmx.de>
2026-03-13 17:04:57 +0100

    Hw13

---
+++ src/test/java/com/ait/tests/Hw_13_FindElementsTests.java
@@ -0,0 +1,44 @@
+package com.ait.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Hw_13_FindElementsTests {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void hw_13_findElementByidTest(){
+        driver.findElement(By.id("dialog-notifications-success"));
+
+
+
+    }
+    @Test
+    public void hw_13_findElementByTegName(){
+        WebElement body = driver.findElement(By.tagName("body"));
+        System.out.println(body.isDisplayed());
+
+    }
+    @Test
+    public void hw_13_findElementByClassName(){
+        WebElement footer = driver.findElement(By.className("footer"));
+        System.out.println(footer.isDisplayed());
+    }
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        driver.quit();
+    }
+}
